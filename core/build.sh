acf2be2fa81fb8bf772768321dae898e786821aa
chenyu <believe.chenyu@gmail.com>
2021-10-30 22:27:58 +0800

    Update core server

--- core/build.sh
+++ core/build.sh
@@ -1,6 +1,9 @@
 set -ex
 
+cd `dirname $0`
+
 bazel build --config=c++17 -c opt //main:server //main:client
 
 OUT_PATH=bazel-out/k8-opt/bin
+rm -rf ./server ./client
 cp $OUT_PATH/main/server $OUT_PATH/main/client ./
